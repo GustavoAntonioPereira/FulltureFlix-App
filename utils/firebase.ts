06de3d653e04acb21ecd825ceb3c25bc57697ee3
GustavoAntonioPereira <guga-vivi@hotmail.com>
2024-06-07 19:44:47 -0300

    commit de dependencias

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,13 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 // Optionally import the services that you want to use
-// import {...} from "firebase/auth";
+import { getAuth } from 'firebase/auth';
 // import {...} from "firebase/database";
 // import {...} from "firebase/firestore";
 // import {...} from "firebase/functions";
 // import {...} from "firebase/storage";
 
-// Initialize Firebase
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -20,7 +19,5 @@ const firebaseConfig = {
 };
 
 const firebase = initializeApp(firebaseConfig);
-// For more information on how to access Firebase in your project,
-// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
 
 export default firebase;
