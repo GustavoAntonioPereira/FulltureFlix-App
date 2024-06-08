535f1a94e056ac7151f111f06a729a187eb4a313
GustavoAntonioPereira <guga-vivi@hotmail.com>
2024-06-08 09:30:01 -0300

    "ajust do firebase"

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,23 +1,19 @@
+// firebaseConfig.ts
 import { initializeApp } from 'firebase/app';
-
-// Optionally import the services that you want to use
 import { getAuth } from 'firebase/auth';
-// import {...} from "firebase/database";
-// import {...} from "firebase/firestore";
-// import {...} from "firebase/functions";
-// import {...} from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
+  databaseURL: process.env.EXPO_PUBLIC_DATABASE_URL,
   projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID,
 };
 
 const firebase = initializeApp(firebaseConfig);
+const auth = getAuth(firebase);
 
-export default firebase;
+export { firebase, auth };
